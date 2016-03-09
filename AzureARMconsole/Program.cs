0091a96dd42aea5d8eb48329596841fb7a978c89
Leif Elgaard Høj <leif_h5@hotmail.com>
2016-03-09 09:54:51 +0100

    1

---
+++ AzureARMconsole/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure;
+
+using Microsoft.Azure.Management.Resources;
+using Microsoft.Azure.Management.Resources.Models;
+using Microsoft.Rest;
+
+
+namespace AzureARMconsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+}
