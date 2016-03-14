990462563e0cefb8cde6c784ecf60934e54613c2
Leif Elgaard Høj <leif_h5@hotmail.com>
2016-03-14 17:06:23 +0100

    postgress

--- GinsaGroup/CustomScripts/postgresinstall.ps1
+++ GinsaGroup/CustomScripts/postgresinstall.ps1
@@ -8,7 +8,241 @@
 ## Installer provided by EnterpriseDB: http://www.enterprisedb.com/company/about-enterprisedb
 ################################################################################
 
+Param
+(
+    [Parameter(Mandatory=$true)]
+    [Alias('User')][String]$pgUser,
+
+    [Parameter(Mandatory=$true)]
+    [Alias('Password')][String]$pgPassword,
+
+    [Parameter(Mandatory=$false)]
+    [Alias('InstallerUrl')][String]$pgKitSource="http://get.enterprisedb.com/postgresql/postgresql-9.3.5-1-windows-x64.exe",
+
+    [Parameter(Mandatory=$false)]
+    [Alias('InstallPath')][String]$pgInstallPath="C:\Program Files\PostgreSQL\9.3",
+
+    [Parameter(Mandatory=$false)]
+    [Alias('DataPath')][String]$pgDataPath="C:\Program Files\PostgreSQL\9.3\data",
+
+    [Parameter(Mandatory=$false)]
+    [Alias('Locale')][String]$pgLocale="English, United States",
+
+    [Parameter(Mandatory=$false)]
+    [Alias('Port')][int]$pgPort=5432,
+
+    [Parameter(Mandatory=$false)]
+    [Alias('ServiceName')][String]$pgServiceName="postgresql"
+)
+BEGIN{
+
 Write-Host "Install-Postgres Module loaded" -ForegroundColor DarkGreen
+Function Set-Owner {
+    <#
+        .SYNOPSIS
+            Changes owner of a file or folder to another user or group.
+
+        .DESCRIPTION
+            Changes owner of a file or folder to another user or group.
+
+        .PARAMETER Path
+            The folder or file that will have the owner changed.
+
+        .PARAMETER Account
+            Optional parameter to change owner of a file or folder to specified account.
+
+            Default value is 'Builtin\Administrators'
+
+        .PARAMETER Recurse
+            Recursively set ownership on subfolders and files beneath given folder.
+
+        .NOTES
+            Name: Set-Owner
+            Author: Boe Prox
+            Version History:
+                 1.0 - Boe Prox
+                    - Initial Version
+
+        .EXAMPLE
+            Set-Owner -Path C:\temp\test.txt
+
+            Description
+            -----------
+            Changes the owner of test.txt to Builtin\Administrators
+
+        .EXAMPLE
+            Set-Owner -Path C:\temp\test.txt -Account 'Domain\bprox
+
+            Description
+            -----------
+            Changes the owner of test.txt to Domain\bprox
+
+        .EXAMPLE
+            Set-Owner -Path C:\temp -Recurse 
+
+            Description
+            -----------
+            Changes the owner of all files and folders under C:\Temp to Builtin\Administrators
+
+        .EXAMPLE
+            Get-ChildItem C:\Temp | Set-Owner -Recurse -Account 'Domain\bprox'
+
+            Description
+            -----------
+            Changes the owner of all files and folders under C:\Temp to Domain\bprox
+    #>
+    [cmdletbinding(
+        SupportsShouldProcess = $True
+    )]
+    Param (
+        [parameter(ValueFromPipeline=$True,ValueFromPipelineByPropertyName=$True)]
+        [Alias('FullName')]
+        [string[]]$Path,
+        [parameter()]
+        [string]$Account = 'Builtin\Administrators',
+        [parameter()]
+        [switch]$Recurse
+    )
+    Begin {
+        #Prevent Confirmation on each Write-Debug command when using -Debug
+        If ($PSBoundParameters['Debug']) {
+            $DebugPreference = 'Continue'
+        }
+        Try {
+            [void][TokenAdjuster]
+        } Catch {
+            $AdjustTokenPrivileges = @"
+            using System;
+            using System.Runtime.InteropServices;
+
+             public class TokenAdjuster
+             {
+              [DllImport("advapi32.dll", ExactSpelling = true, SetLastError = true)]
+              internal static extern bool AdjustTokenPrivileges(IntPtr htok, bool disall,
+              ref TokPriv1Luid newst, int len, IntPtr prev, IntPtr relen);
+              [DllImport("kernel32.dll", ExactSpelling = true)]
+              internal static extern IntPtr GetCurrentProcess();
+              [DllImport("advapi32.dll", ExactSpelling = true, SetLastError = true)]
+              internal static extern bool OpenProcessToken(IntPtr h, int acc, ref IntPtr
+              phtok);
+              [DllImport("advapi32.dll", SetLastError = true)]
+              internal static extern bool LookupPrivilegeValue(string host, string name,
+              ref long pluid);
+              [StructLayout(LayoutKind.Sequential, Pack = 1)]
+              internal struct TokPriv1Luid
+              {
+               public int Count;
+               public long Luid;
+               public int Attr;
+              }
+              internal const int SE_PRIVILEGE_DISABLED = 0x00000000;
+              internal const int SE_PRIVILEGE_ENABLED = 0x00000002;
+              internal const int TOKEN_QUERY = 0x00000008;
+              internal const int TOKEN_ADJUST_PRIVILEGES = 0x00000020;
+              public static bool AddPrivilege(string privilege)
+              {
+               try
+               {
+                bool retVal;
+                TokPriv1Luid tp;
+                IntPtr hproc = GetCurrentProcess();
+                IntPtr htok = IntPtr.Zero;
+                retVal = OpenProcessToken(hproc, TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY, ref htok);
+                tp.Count = 1;
+                tp.Luid = 0;
+                tp.Attr = SE_PRIVILEGE_ENABLED;
+                retVal = LookupPrivilegeValue(null, privilege, ref tp.Luid);
+                retVal = AdjustTokenPrivileges(htok, false, ref tp, 0, IntPtr.Zero, IntPtr.Zero);
+                return retVal;
+               }
+               catch (Exception ex)
+               {
+                throw ex;
+               }
+              }
+              public static bool RemovePrivilege(string privilege)
+              {
+               try
+               {
+                bool retVal;
+                TokPriv1Luid tp;
+                IntPtr hproc = GetCurrentProcess();
+                IntPtr htok = IntPtr.Zero;
+                retVal = OpenProcessToken(hproc, TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY, ref htok);
+                tp.Count = 1;
+                tp.Luid = 0;
+                tp.Attr = SE_PRIVILEGE_DISABLED;
+                retVal = LookupPrivilegeValue(null, privilege, ref tp.Luid);
+                retVal = AdjustTokenPrivileges(htok, false, ref tp, 0, IntPtr.Zero, IntPtr.Zero);
+                return retVal;
+               }
+               catch (Exception ex)
+               {
+                throw ex;
+               }
+              }
+             }
+"@
+            Add-Type $AdjustTokenPrivileges
+        }
+
+        #Activate necessary admin privileges to make changes without NTFS perms
+        [void][TokenAdjuster]::AddPrivilege("SeRestorePrivilege") #Necessary to set Owner Permissions
+        [void][TokenAdjuster]::AddPrivilege("SeBackupPrivilege") #Necessary to bypass Traverse Checking
+        [void][TokenAdjuster]::AddPrivilege("SeTakeOwnershipPrivilege") #Necessary to override FilePermissions
+    }
+    Process {
+        ForEach ($Item in $Path) {
+            Write-Verbose "FullName: $Item"
+            #The ACL objects do not like being used more than once, so re-create them on the Process block
+            $DirOwner = New-Object System.Security.AccessControl.DirectorySecurity
+            $DirOwner.SetOwner([System.Security.Principal.NTAccount]$Account)
+            $FileOwner = New-Object System.Security.AccessControl.FileSecurity
+            $FileOwner.SetOwner([System.Security.Principal.NTAccount]$Account)
+            $DirAdminAcl = New-Object System.Security.AccessControl.DirectorySecurity
+            $FileAdminAcl = New-Object System.Security.AccessControl.DirectorySecurity
+            $AdminACL = New-Object System.Security.AccessControl.FileSystemAccessRule('Builtin\Administrators','FullControl','ContainerInherit,ObjectInherit','InheritOnly','Allow')
+            $FileAdminAcl.AddAccessRule($AdminACL)
+            $DirAdminAcl.AddAccessRule($AdminACL)
+            Try {
+                $Item = Get-Item -LiteralPath $Item -Force -ErrorAction Stop
+                If (-NOT $Item.PSIsContainer) {
+                    If ($PSCmdlet.ShouldProcess($Item, 'Set File Owner')) {
+                        Try {
+                            $Item.SetAccessControl($FileOwner)
+                        } Catch {
+                            Write-Warning "Couldn't take ownership of $($Item.FullName)! Taking FullControl of $($Item.Directory.FullName)"
+                            $Item.Directory.SetAccessControl($FileAdminAcl)
+                            $Item.SetAccessControl($FileOwner)
+                        }
+                    }
+                } Else {
+                    If ($PSCmdlet.ShouldProcess($Item, 'Set Directory Owner')) {                        
+                        Try {
+                            $Item.SetAccessControl($DirOwner)
+                        } Catch {
+                            Write-Warning "Couldn't take ownership of $($Item.FullName)! Taking FullControl of $($Item.Parent.FullName)"
+                            $Item.Parent.SetAccessControl($DirAdminAcl) 
+                            $Item.SetAccessControl($DirOwner)
+                        }
+                    }
+                    If ($Recurse) {
+                        [void]$PSBoundParameters.Remove('Path')
+                        Get-ChildItem $Item -Force | Set-Owner @PSBoundParameters
+                    }
+                }
+            } Catch {
+                Write-Warning "$($Item): $($_.Exception.Message)"
+            }
+        }
+    }
+    End {  
+        #Remove priviledges that had been granted
+        [void][TokenAdjuster]::RemovePrivilege("SeRestorePrivilege") 
+        [void][TokenAdjuster]::RemovePrivilege("SeBackupPrivilege") 
+        [void][TokenAdjuster]::RemovePrivilege("SeTakeOwnershipPrivilege")     
+    }
+}
 
 Function Install-Postgres
 {
@@ -87,13 +321,12 @@ You need to have administrative permissions to run this script.
         [Parameter(Mandatory=$false)]
         [Alias('ServiceName')][String]$pgServiceName="postgresql"
     )
-
+	Begin{
     $pgKit = "$PSScriptRoot\postgresql.exe";
     $pgConfigSource = "$PSScriptRoot\config";
     $pgPassPath = "C:\Users\$pgUser\AppData\Roaming\postgresql";
 
     Write-Host "Starting...";
-    break;
 
     Write-Host "Creating local user $pgUser";
 
@@ -215,6 +448,7 @@ You need to have administrative permissions to run this script.
 
     Write-Host "Postgres has been installed";
 }
+}
 
 
 function New-LocalUser($userName, $password)
@@ -334,5 +568,12 @@ function Install-WebServer()
     # List Web features 
     Get-WindowsFeature | where {$_.Name -like "Web*"}
 }
-Install-Postgres -User "postgres" -Password "ChangeMe!" -InstallUrl "http://get.enterprisedb.com/postgresql/postgresql-9.3.5-1-windows-x64.exe" -InstallPath "C:\Program Files\PostgreSQL\9.3" -DataPath "C:\Program Files\PostgreSQL\9.3\data" -Locale "Romanian, Romania" -Port 5432 -ServiceName "postgresql"
+
+}
+Process{
+	Write-Output $pgPassword
+	Install-Postgres -User $pgUser -Password $pgPassword -InstallerUrl $pgKitSource -InstallPath $pgInstallPath -DataPath $pgDataPath -Locale $pgLocale -Port $pgPort-ServiceName $pgServiceName
+}
+END{
+}
 
